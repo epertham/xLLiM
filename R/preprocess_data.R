8d9185dab7c53e806d994400ea3d2c7d1a17d996
Emeline Perthame <emeline.perthame@pasteur.fr>
2020-06-24 14:54:00 +0200

    preprocess_data

--- R/preprocess_data.R
+++ R/preprocess_data.R
@@ -1,13 +1,12 @@
-preprocess_data = function(tapp,yapp,in_K,alpha,nfolds){
+preprocess_data = function(tapp,yapp,in_K,...){
   init.kmeans = kmeans(cbind(tapp,yapp),in_K)
   ind = c()
   for (k in 1:in_K){
     cv <- cv.glmnet(as.matrix(yapp[init.kmeans$cluster== k,]),
-                    as.matrix(tapp[init.kmeans$cluster== k,]),family="mgaussian", 
-                    alpha=alpha,nfolds = nfolds)
+                    as.matrix(tapp[init.kmeans$cluster== k,]),family="mgaussian",...)
     mod <- glmnet(as.matrix(yapp[init.kmeans$cluster== k,]),
                   as.matrix(tapp[init.kmeans$cluster== k,]),family="mgaussian",
-                  lambda=cv$lambda.1se, alpha=alpha)
+                  lambda=cv$lambda.1se, ....)
     for (l in 1:dim(tapp)[2]){
       ind = c(ind, which(mod$beta[[l]] !=0))
     }
