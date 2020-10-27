55ffda6088e96a9ae8e7213b0d90e98f16300308
epertham <emeline.perthame@pasteur.fr>
2020-10-27 09:48:19 +0100

    maj pre process function

--- R/preprocess_data.R
+++ R/preprocess_data.R
@@ -2,19 +2,38 @@ preprocess_data = function(tapp,yapp,in_K,...){
   init.kmeans = kmeans(cbind(tapp,yapp),in_K)
   ind = c()
   for (k in 1:in_K){
+    print(paste0("k=",k))
+    
     cv <- cv.glmnet(as.matrix(yapp[init.kmeans$cluster== k,]),
                     as.matrix(tapp[init.kmeans$cluster== k,]),family="mgaussian",...)
     mod <- glmnet(as.matrix(yapp[init.kmeans$cluster== k,]),
                   as.matrix(tapp[init.kmeans$cluster== k,]),family="mgaussian",
                   lambda=cv$lambda.1se, ...)
-    indk <- sapply(mod$beta, function(x){which(x != 0)})
+    indk <- c()
+    for (l in 1:dim(tapp)[2]){
+      indk = c(indk, which(mod$beta[[l]] !=0))
+    }
     indk <- unique(indk)
+    print(paste0("indk1=",indk))
+    print(paste0("length indk1=",length(indk)))
+    
     if (length(indk) == 0){
-      nzero.lambda <- max(cv$lambda[cv$nzero > 0])
+      print(paste0("nzero",c(min(cv$nzero),max(cv$nzero))))
       mod <- glmnet(as.matrix(yapp[init.kmeans$cluster== k,]),
                     as.matrix(tapp[init.kmeans$cluster== k,]),family="mgaussian",
-                    lambda=nzero.lambda, ...)
+                    lambda=cv$lambda, ...)
+      mod <- glmnet(as.matrix(yapp[init.kmeans$cluster== k,]),
+                    as.matrix(tapp[init.kmeans$cluster== k,]),family="mgaussian",
+                    lambda=max(cv$lambda[mod$dfmat[1,] >= 1]), ...)
+      indk <- c()
+      for (l in 1:dim(tapp)[2]){
+        indk = c(indk, which(mod$beta[[l]] !=0))
+      }
+      indk <- unique(indk)
+      print(paste0("indk2=",indk))
+      print(paste0("length indk2=",length(indk)))
     }
+    
     for (l in 1:dim(tapp)[2]){
       ind = c(ind, which(mod$beta[[l]] !=0))
     }
